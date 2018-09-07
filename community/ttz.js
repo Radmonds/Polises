f9357434283de392c36ef7647c85d4c6ee83736f
Oilburner09 <43079596+Oilburner09@users.noreply.github.com>
2018-09-07 16:12:11 -0700

    Create ttz.js
    TTZ User and channel customization

---
+++ community/ttz.js
@@ -0,0 +1,30 @@
+(function() {
+   
+            users = {
+                'gigaho': 'fundamental-analysts',
+                'Duane\.Norman': 'fundamental-analysts',
+                'MoonBear': 'shorthorns',
+                'Dylan': 'bulls',
+                'Epistemic\_Crisis': 'shorthorns',
+                'gSTRUCTOR': 'shorthorns',
+                'Justin': 'shorthorns',
+                'joje': 'shorthorns',
+                'Mihai': 'shorthorns'
+            };
+             $(document).ready().each(users, function(key, value) {
+                $('[data-username="' + key + '"]').addClass(value);
+            });
+
+            //TTZ Channels
+            channel = {
+                "3KeG6kuRACiXhXEEK": "ttz-channels",
+                "H8u3WA8zv6nToxW": "ttz-channels",
+                "NooXCRNnu6aisXrzD": "ttz-channels",
+                "8QqdmnNB9stcsL77L": "ttz-channels",
+                "zoHqJLnHjbGdefJym": "ttz-channels",
+                "AerGuebK7A2S93Mjb": "ttz-channels"
+            }
+            $(document).ready().each(channel, function(key, value) {
+                $('section#chat-window-' + key).addClass(value);
+            });
+});
